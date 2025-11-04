4503f6aec6f869a1847a4115db71bbbf3dbb4c17
Namyamb <namya.shah@mobiosolutions.com>
2025-11-04 19:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -528,7 +528,7 @@ def connect_to_drive():
 
 # ✅ Replace these IDs with your real Google Drive folder IDs
 DRIVE_IDS = {
-    "recordings": "1cXknT8JR2VTCsVk0w_d0YGMgIVHLv-8y",
+    "recordings": "1Ji6B8ejAt93W0smT0bsdFpXcr04EHbtC",
     "new_samples": "1cXknT8JR2VTCsVk0w_d0YGMgIVHLv-8y",
     "backups": "1LhOyaEWH0x1w7jr2sg8gyv5TXn0dtChi",
     "root": "1SCcEDnUm_5cDRtnuIxfWfr2fWS5KOzXQ"
